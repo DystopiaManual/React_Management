4cc3811826e24e94b2acd46777704949d1ad4883
DevJung <nsw99@hanmail.net>
2023-05-09 07:43:57 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+  id: 1,
+  image: 'https://placeimg.com/64/64/1',
+  name: '홍길동',
+  birthday: '961222',
+  gender: '남자',
+  job: '대학생'
+},
+  {
+  id: 2,
+  image: 'https://placeimg.com/64/64/2',
+  name: '김태리',
+  birthday: '951127',
+  gender: '여자',
+  job: '대학생'
+},
+  {
+  id: 3,
+  image: 'https://placeimg.com/64/64/3',
+  name: '김우일',
+  birthday: '861222',
+  gender: '남자',
+  job: '통역사'
+},
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
